f23230eb5636efdcefe50088d4ce8beccd8a85f5
Andrew Glenn <glennan@oregonstate.edu>
2023-05-24 11:07:40 -0500

    updated to match live version

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+//This file was adapted from CS340's nodejs starter app repo.
+
+
 // App.js
 /*
     SETUP
@@ -24,11 +27,47 @@ app.get('/index', function(req, res)
     {
         res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
     });                                         // will process this file, before sending the finished HTML to the client.
+
+app.get('/comments', function(req, res)
+    {
+        res.render('comments');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });
+
+app.get('/edit-comment', function(req, res)
+    {
+        res.render('edit-comment');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });
+
+app.get('/edit-hashtag', function(req, res)
+    {
+        res.render('edit-hashtag');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });
+
+app.get('/edit-user', function(req, res)
+    {
+        res.render('edit-user');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });
+
+app.get('/hashtags', function(req, res)
+    {
+        res.render('hashtags');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });
+
+app.get('/post-details', function(req, res)
+    {
+        res.render('post-details');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });
+
+app.get('/users', function(req, res)
+    {
+        res.render('users');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });
+
 app.get('/posts', function(req, res)
     {
         //query db on page load
         let query1 = "SELECT Posts.post_id, Users.name, Posts.contents FROM Posts Inner Join Users ON Posts.author = Users.user_id;";    //define query for posts page
-
+        console.log(query1);
         db.pool.query(query1, function(error, rows, fields){    //Execute the query
 
             res.render('posts', {data:rows});   // Render the posts.hbs file, and also send the renderer
@@ -36,6 +75,166 @@ app.get('/posts', function(req, res)
                                                 // received back from the query
         })
     });
+
+app.get('/edit-post', function(req, res) {
+    let postID = parseInt(req.query.id); // Retrieve the post ID from the query parameter
+    
+    // fetch the posts data with author names
+    let queryPosts = "SELECT Posts.post_id, Users.name, Posts.contents FROM Posts INNER JOIN Users ON Posts.author = Users.user_id;";
+    
+    //  fetch all registered users
+    let queryUsers = "SELECT * FROM Users;";
+    
+    // Execute the first query to fetch posts data
+    db.pool.query(queryPosts, function(error, postRows, fields) {
+        if (error) {
+        console.log(error);
+        res.sendStatus(400);
+        return;
+        }
+    
+        // Execute the second query to fetch users data
+        db.pool.query(queryUsers, function(error, userRows, fields) {
+        if (error) {
+            console.log(error);
+            res.sendStatus(400);
+            return;
+        }
+    
+        // Find the selected post based on the post ID
+        let selectedPost = postRows.find(post => post.post_id === postID);
+        if (!selectedPost) {
+            // If the selected post is not found, handle the error accordingly
+            res.sendStatus(404);
+            return;
+        }
+    
+        // Render the edit-post template with data
+        res.render('edit-post', {
+            data: postRows, // Pass posts data 
+            users: userRows, // Pass users data 
+            postID: postID, // Pass the post ID
+            selectedContents: selectedPost.contents // Pass the selected post's contents
+            });
+        });
+    });
+});
+      
+// app.js
+
+app.post('/add-post-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Get user ID
+    let getUserQuery = `SELECT user_id FROM Users WHERE name = '${data['postAuthor']}'`;
+
+    // Run query
+    db.pool.query(getUserQuery, function(error, rows, fields){
+        if (error) {
+            window.alert('User not found.');
+            res.sendStatus(400);
+        } else {
+            if (rows.length > 0){
+                // Get user ID from query result
+                let userId = rows[0].user_id;
+
+                // Capture NULL values
+                let hashtags = parseInt(data['postHashtags']);
+                if (isNaN(hashtags))
+                {
+                    hashtags = 'NULL'
+                }
+                // Create the query and run it on the database
+                query1 = `INSERT INTO Posts (author, contents) VALUES (${userId}, '${data['postContent']}')`;
+                db.pool.query(query1, function(error, rows, fields){
+
+                    // Check to see if there was an error
+                    if (error) {
+
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error)
+                        res.sendStatus(400);
+                    }
+
+                    // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+                    // presents it on the screen
+                    else
+                    {
+                        res.redirect('/posts');
+                    }
+                });
+
+            } else {
+                window.alert('User not found.');
+                res.sendStatus(400);
+            }
+        }
+    });
+});
+
+app.delete('/delete-post-ajax/', function(req,res,next){
+    let data = req.body;
+    let postID = parseInt(data.id);
+    let deletePosts_Hashtags = `DELETE FROM Posts_Hashtags WHERE post_id = ?`;
+    let deletePost= `DELETE FROM Posts WHERE post_id = ?`;
+  
+  
+          // Run the 1st query
+          db.pool.query(deletePosts_Hashtags, [postID], function(error, rows, fields){
+              if (error) {
+  
+              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+              console.log(error);
+              res.sendStatus(400);
+              }
+  
+              else
+              {
+                  // Run the second query
+                  db.pool.query(deletePost, [postID], function(error, rows, fields) {
+  
+                      if (error) {
+                          console.log(error);
+                          res.sendStatus(400);
+                      } else {
+                          res.sendStatus(204);
+                      }
+                  })
+              }
+  })});
+
+  app.put('/put-post-ajax', function (req, res, next) {
+    let data = {
+        postAuthor: parseInt(req.body.postAuthor),
+        postContent: req.body.postContent
+    };
+
+    let queryUpdatePost = `UPDATE Posts SET author = ?, contents = ? WHERE post_id = ?`;
+    let selectPosts = `SELECT * FROM Posts;`;
+
+    // Run the 1st query
+    db.pool.query(queryUpdatePost, [data.postAuthor, data.postContent, req.query.id], function (error, rows, fields) {
+        if (error) {
+            // Log the error 
+            console.log("Update Error:", error);
+            res.sendStatus(400);
+        } else {
+            // Run the second query
+            db.pool.query(selectPosts, function (error, rows, fields) {
+                if (error) {
+                    // Log the error 
+                    console.log("Select Error:", error);
+                    res.sendStatus(400);
+                } else {
+                    res.send(rows);
+                }
+            });
+        }
+    });
+});
+
+
 
 /*
     LISTENER
